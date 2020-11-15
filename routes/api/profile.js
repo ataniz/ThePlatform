8e94237c7bb75143776a2579ae4fc5eba059a1ea
Ata Nizamoglu <nizamoglu@campus.tu-berlin.de>
2020-11-15 15:23:27 +0100

    posts route w likes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -113,4 +113,61 @@ router.post(
   }
 );
 
+// @route GET api/profile
+// @desc Get all user profiles
+// @access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user ID
+// @access Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+
+    // if the id is invalid respond with not found
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route Delete api/profile
+// @desc Delete User, User Profile and Posts (Not sure about the posts maybe add a admin approval)
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Delete User's Profile
+    const profile = await Profile.findOneAndRemove({ user: req.user.id });
+    // Here findOneAndRemove and findOneAndDelete are both could be used?
+    // Delete User
+    const user = await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User Removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
